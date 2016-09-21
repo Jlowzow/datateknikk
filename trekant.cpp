ae8d4bb21209a1f1287b6e60a551cb41223b8930
Jlowzow <joachim@lowzow.org>
2016-09-21 18:13:44 +0200

    modified:   trekant.cpp

--- trekant.cpp
+++ trekant.cpp
@@ -8,13 +8,55 @@
 using namespace std;
 
 static double PI = acos(-1);
+bool stoppkode = false;
 
-double trekantAreal(double l, double h){
-    double a = (l * h) / 2;
-    return a;
+double trekantAreal(double, double, double);
+double lesInn(string);
+vector<double> trekantVinkler(double, double, double);
+bool triangel(double, double, double);
+void intro();
+
+int main(){
+    
+    intro();
+
+    while(!stoppkode){
+        double a, b, c;
+        a = lesInn("a");
+        b = lesInn("b");
+        c = lesInn("c");
+        
+        cout << endl;
+
+        if (triangel(a, b, c)){
+            vector<double> vinkler = trekantVinkler(a, b, c);
+            
+            cout << "Vinklene i trekanten er: " << endl;
+
+
+//          for(double n : vinkler){ Den enkleste måten å loope gjennom en container, virker ikke før C++11. plz fix
+            for(int n = 0; n < vinkler.size(); n++){
+                cout << vinkler[n] * 180 / PI << '\n';
+            }
+            cout << endl;   
+            cout << "Arealet er: " << trekantAreal(a, b, c) << endl; 
+
+
+        } else {
+
+            cout << "Dette er umulig en triangel." << endl;
+        }
+    }
 }
 
-vector<double>  trekantVinkler(double a, double b, double c){
+double trekantAreal(double a, double b, double c){ //Bruker herons formel til å regne ut arealet
+    double s = (a + b + c) / 2;
+    double A = sqrt( s * (s-a) * (s-b) * (s-c));
+    return A;
+}
+
+vector<double>  trekantVinkler(double a, double b, double c){ //Benytter cosinus setningen til å regne ut vinkler
+                                                              //Returnerer en vector med verdiene.
     vector<double> vinkler(3);
    
 
@@ -28,39 +70,28 @@ vector<double>  trekantVinkler(double a, double b, double c){
 
     return vinkler;
 }
-
+// Får vinklene fra brukeren, setter stoppBit til 1 om den får en 0.
 double lesInn(string melding){
     cout << melding + ": ";
     double a;
     cin >> a; 
-
+    if(a == 0){
+        stoppkode = true;}
     return a;
 }
-
+// Funksjon som tester om dette er en triangel.
 bool triangel(double a,double b,double c){
     bool x = true;
 
     if(a + b < c || a + c < b || b + c < a){
         x = false;
     }
-
+    if(a == 0 | b == 0 | c == 0){
+        x = false;
+    }
     return x;
 }
 
-int main(){
-    double a, b, c;
-    a = lesInn("a");
-    b = lesInn("b");
-    c = lesInn("c");
-    
-    if (triangel(a, b, c)){
-        vector<double> vinkler = trekantVinkler(a, b, c);
-        for(double n : vinkler){
-            cout << n * 180 / PI << '\n';
-        }
-    } else {
-        cout << "Dette er umulig en triangel." << endl;
-    }
-
-    
+void intro(){
+    cout << "Dette programmet finner vinklene (i grader) og arealet til en trekant. Skriv inn lengdene, avslutt ved a la en side være 0." << endl;
 }
