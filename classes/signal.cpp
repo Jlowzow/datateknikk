ca0da7e71eaa32845497d554cc4a46a27639d272
Jlowzow <joachim@lowzow.org>
2016-10-07 17:08:44 +0200

    new file:   signal.cpp 	new file:   signal.h

---
+++ classes/signal.cpp
@@ -0,0 +1,20 @@
+#include "signal.h"
+#include <math.h>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+double dToRad(double d){
+    double rad;
+    rad = d *  PI / 180;
+    return rad;
+}
+
+double noise(double in){
+    srand(time(NULL));
+    double out;
+    out = in + 0.1 * ((double) rand() / RAND_MAX - 0.5);
+    return out;
+}
---
+++ classes/signal.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_signal_h
+#define GUARD_signal_h
+
+#include <math.h>
+
+static const double PI = acos(-1);
+
+double dToRad(double);
+double noise(double);
+
+#endif
